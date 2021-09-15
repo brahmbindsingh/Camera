dad0d7cf219a20ef18c39e81abdd0434157d2a32
Brahm Bind Singh <singhbrahmbind@gmail.com>
2021-09-15 17:46:06 +0530

    day2

--- script.js
+++ script.js
@@ -3,15 +3,71 @@ let mediaRecorder;
 let chunks = [];
 let isRecording = false;
 let recordBtn = document.querySelector("#record");
+let captureBtn = document.querySelector("#capture");
+let body = document.querySelector("body");
+let filter = "";
+
+let allFilters = document.querySelectorAll(".filter");
+
+for (let i = 0; i < allFilters.length; i++) {
+  allFilters[i].addEventListener("click", function (e) {
+    let previousFilter = document.querySelector(".filter-div");
+
+    if (previousFilter) previousFilter.remove();
+
+    let color = e.currentTarget.style.backgroundColor;
+    filter = color;
+    let div = document.createElement("div");
+    div.classList.add("filter-div");
+    div.style.backgroundColor = color;
+    body.append(div);
+  });
+}
+
+captureBtn.addEventListener("click",()=>{
+    let innerSpan = captureBtn.querySelector("span");
+
+    innerSpan.classList.add("capture-animation");
+    setTimeout(function () {
+        innerSpan.classList.remove("capture-animation");
+    }, 1000);
+
+    let canvas = document.createElement("canvas");
+    canvas.width = videoPlayer.videoWidth;
+    canvas.height = videoPlayer.videoHeight;
+
+    let tool = canvas.getContext("2d");
+    tool.drawImage(videoPlayer,0,0);
+    if (filter != "") {
+        tool.fillStyle = filter;
+        tool.fillRect(0, 0, canvas.width, canvas.height);
+    }
+    let url = canvas.toDataURL();
+    canvas.remove();
+    // body.append(canvas);
+    let anchor = document.createElement("a");
+    anchor.href = url;
+    anchor.download = "image.png";
+    anchor.click();
+    anchor.remove();
+});
 
 recordBtn.addEventListener("click",()=>{
+    let innerSpan = recordBtn.querySelector("span");
+
+    let previousFilter = document.querySelector(".filter-div");
+    if (previousFilter) previousFilter.remove();
+    filter = "";
+    
     if(isRecording){
         mediaRecorder.stop();
         isRecording = false;
+        innerSpan.classList.remove("record-animation");
     }
     else{
         mediaRecorder.start();
         isRecording = true;
+        innerSpan.classList.add("record-animation");
     }
 })
 
@@ -39,6 +95,7 @@ promiseToUseCamera
             anchor.href = link;
             anchor.download = "video.mp4";
             anchor.click();
+            anchor.remove();
         });
 
         
